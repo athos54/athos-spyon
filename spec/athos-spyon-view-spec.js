2341b4343d2e47e0fa0e96fc087927f3e1d291aa
Athos Orio <athos.oc@gmail.com>
2018-12-20 12:59:28 +0100

    first commit

---
+++ spec/athos-spyon-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AthosSpyonView from '../lib/athos-spyon-view';
+
+describe('AthosSpyonView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
